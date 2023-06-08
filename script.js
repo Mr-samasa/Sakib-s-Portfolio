d3d683bc8d7beedb7effb4cc11347a06df5f29a5
Mr-samasa <92912288+Mr-samasa@users.noreply.github.com>
2023-06-08 20:13:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed (".multiple-text",{
+	strings: ["Video Editor","Video Editor","YouTuber",
+	"Editing Specialist"],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true,
+})
